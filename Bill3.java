93f3d1633498001681e1b8ed8b709a6fd01d63db
tomer333 <tomer.dahan71@gmail.com>
2024-11-05 19:58:27 +0200

    Completed HomeWork First Check

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,14 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
+
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		int price = Integer.parseInt(args[3]);
+		double priceDouble = price;
+		priceDouble = Math.ceil(priceDouble/3);
+		System.out.println("Dear "+name1+", "+name2+", and "+name3+": pay "+priceDouble+" Shekels each.");
+  
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,8 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int i = Integer.parseInt(args[0]);
+		System.out.println(i/100+" hundreds, "+(i%100)/10+" tens, "+i%10+" ones.");
+
 	}
 }
